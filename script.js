3643b1cbb59fcba5db4e6b7e71fe297e313da54e
Flavia Prezzi <prezziflavia@gmail.com>
2023-10-16 20:02:02 -0300

    Atualizando imagens

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 function toggMode() {
     const html = document.documentElement
-
-    //if (html.classList.contains('light')) {
-    //    html.classList.remove('light')
-   // } else{
-   //     html.classList.add('light')
-   // }
    html.classList.toggle('light')
 
    //pegar a tag image
@@ -14,7 +8,6 @@ function toggMode() {
     //Se tiver light mode add a imagem light
    //susbtituir a imagem
    //se tiver sem light mode, manter a imagem
-   
    const img= document.querySelector("#profile img")
    if (html.classList.contains('light')) {
         img.setAttribute('src', './imagens/avatar-light.png' )
